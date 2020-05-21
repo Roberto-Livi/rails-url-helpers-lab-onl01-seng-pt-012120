c1b24e1e1c45a9212c0c80e48ad90ce6bf482ebf
Roberto Livi <robertomlivi@outlook.com>
2020-05-21 01:43:40 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,18 @@ class StudentsController < ApplicationController
 
   def show
   end
+  
+  def activate
+    @student = Student.find(params[:id])
+    
+    if @student.active?
+      @student.active = true
+    else
+      @student.active = false
+    end
+  @student.save
+  
+  end
 
   private
 
